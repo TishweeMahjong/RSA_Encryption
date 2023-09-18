759925adcb4f5c91f846872c2a06091a0cdb1606
Atishree <atishreed@gmail.com>
2023-09-18 19:36:36 -0400

    Refine encryption algorithm

--- src/rsa_py.py
+++ src/rsa_py.py
@@ -60,24 +60,3 @@ def decrypt(n, d, encMessage):
     #Convert message back to a string from int if applicable.
 
     return message
-
-
-#TEST METHOD
-"""
-primes = [11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269,271,277,281,283,293,307,311,313,317,331,337,347,349,353,359,367,373,379,383,389,397,401,409,419,421,431,433,439,443,449,457,461,463,467,479,487,491,499,503,509,521,523,541,547,557,563,569,571,577,587,593,599,601,607,613,617,619,631]
-
-trials = 30
-count = 0
-for i in range(trials):
-    p, q = random.sample(primes, 2)
-    n,e,d = rsa_setup(p, q)
-    print(p,q,n,e,d)
-    message = (random.randint(1, n-1))
-    en = encrypt(n, e, message)
-    print(en)
-    if message != decrypt(n, d, en):
-        print("Error for p={0} q={1}".format(p,q))
-        count += 1
-
-print(f"Error rate {count/trials}")
-"""
